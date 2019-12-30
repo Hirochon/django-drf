81628dd39aefa5c82441f56273afd691f83cddf8
Hirochon <heacet43@gmail.com>
2019-12-30 23:34:08 +0900

    [add] Add lib functions

--- book/views.py
+++ book/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import BookForm, BookStockForm
 from django.views import View
 from .models import Book, BookStock
+from django.db.models import Q
 
 class BookView(View):
     def get(self, request, *args, **kwargs):
@@ -28,3 +29,13 @@ class BookStockView(View):
         book = BookStockForm(request.POST, Book())
         book.save()
         return redirect(to='/admin/')
+
+class Bookdesu(View):
+    def get(self, request, *args, **kwargs):
+        params = {
+            'title' : '本一覧',
+            'books' : Book.objects.all().order_by('-created_at'),
+            'bookstocks' : BookStock.objects.all(),
+            'book_pricelesses' : Book.objects.filter(Q(price__gte=1500) & Q(price__lt=2000)),
+        }
+        return render(request, 'book/lib.html', params)
