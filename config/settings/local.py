94c2e5427dae2bd7aa1d74d8ae2f1a7086cd79b5
Hirochon <heacet43@gmail.com>
2019-12-30 12:54:57 +0900

    [add] Add media setting

--- config/settings/local.py
+++ config/settings/local.py
@@ -99,9 +99,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# Media Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 if DEBUG:
     LOGGING = {
