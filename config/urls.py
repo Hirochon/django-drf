f57a261ee10e390bf0a00112bc22b3123877c554
Hirochon <heacet43@gmail.com>
2019-12-31 16:45:52 +0900

    [add] Add path to api

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('book.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('api/', include('api.urls')),
 ]
